41849ec7b2f8e183299893b162469532093717c0
mikezks <23002661+mikezks@users.noreply.github.com>
2023-07-27 17:21:00 +0200

    feat: signals demo

--- libs/tickets/feature-booking/src/lib/flight-search/flight-search.component.ts
+++ libs/tickets/feature-booking/src/lib/flight-search/flight-search.component.ts
@@ -1,9 +1,11 @@
-import { Component, inject } from '@angular/core';
+import { Component, computed, effect, inject, signal } from '@angular/core';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { FlightCardComponent } from '../flight-card/flight-card.component';
 import { CityPipe } from '@flight-demo/shared/ui-common';
 import { Flight, FlightService } from '@flight-demo/tickets/domain';
+import { debounceTime, timer } from 'rxjs';
 
 @Component({
   selector: 'app-flight-search',
@@ -13,17 +15,46 @@ import { Flight, FlightService } from '@flight-demo/tickets/domain';
   imports: [CommonModule, FormsModule, CityPipe, FlightCardComponent],
 })
 export class FlightSearchComponent {
-  from = 'London';
-  to = 'New York';
+  from = signal('London');
+  to = signal('New York');
   flights: Array<Flight> = [];
   selectedFlight: Flight | undefined;
-
+  flightRoute = computed(
+    () => 'From ' + this.from() + ' to ' + this.to() + '.'
+  );
   basket: Record<number, boolean> = {
     3: true,
     5: true,
   };
+  timer$ = timer(0, 1_000);
+  timer = toSignal(this.timer$, {
+    initialValue: -1
+    // requireSync: true
+  });
+  debouncedFrom$ = toObservable(this.from).pipe(
+    debounceTime(300)
+  );
+  debouncedFrom = toSignal(this.debouncedFrom$, {
+    initialValue: ''
+  });
 
   private flightService = inject(FlightService);
+
+  constructor() {
+    this.from.set('Berlin');
+    this.from.set('Paris');
+    this.from.update(city => city + 'Rom');
+
+    const numbers = signal([0]);
+    numbers.mutate(numbers => numbers.push(1));
+
+    setTimeout(() => this.from.set('Madrid'));
+
+    effect(() => {
+      console.log(this.from()/* , this.timer() */);
+    });
+  }
+
 
   search(): void {
     if (!this.from || !this.to) {
@@ -33,7 +64,7 @@ export class FlightSearchComponent {
     // Reset properties
     this.selectedFlight = undefined;
 
-    this.flightService.find(this.from, this.to).subscribe({
+    this.flightService.find(this.from(), this.to()).subscribe({
       next: (flights) => {
         this.flights = flights;
       },
